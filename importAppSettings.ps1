fa9ac946953bcff2164464f76bce493e4adba750
Przemyslaw Konieczny <p.konieczny@hotmail.com>
2025-11-11 10:53:36 +0100

    script

--- importAppSettings.ps1
+++ importAppSettings.ps1
@@ -1,9 +1,10 @@
 [CmdletBinding()]
 param(
-    [string]$Subscription = "NIS.dev1",
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $true, Position = 0)]
     [string]$KeyVaultName,
-    [string]$InputFolder = "$HOME/Desktop/appSettings/dev1"
+    [Parameter(Position = 1)]
+    [string]$InputFolder = "$HOME/Desktop/appSettings/dev1",
+    [string]$Subscription = "NIS.dev1"
 )
 
 if (-not (Get-Module -ListAvailable -Name Az.KeyVault)) {
@@ -43,6 +44,6 @@ foreach ($file in $files) {
             | Out-Null
         Write-Host "Imported $secretName"
     } catch {
-        Write-Warning "Failed to import $secretName: $($_.Exception.Message)"
+        Write-Warning ("Failed to import {0}: {1}" -f $secretName, $_.Exception.Message)
     }
 }
