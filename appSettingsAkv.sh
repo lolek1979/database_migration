fe955b3f48d14d6e55f1458c583023c177d8aa87
Przemyslaw Konieczny <p.konieczny@hotmail.com>
2025-11-11 10:05:52 +0100

    script

---
+++ appSettingsAkv.sh
@@ -0,0 +1,14 @@
+az account set --subscription "NIS.dev1"
+KV_NAME="<your-keyvault-name>"
+OUTPUT_DIR="./kv-secrets"
+
+mkdir -p "$OUTPUT_DIR"
+
+az keyvault secret list \
+  --vault-name "$KV_NAME" \
+  --query '[].id' -o tsv |
+while read -r secret_id; do
+  name=$(basename "$secret_id")
+  value=$(az keyvault secret show --id "$secret_id" --query value -o tsv)
+  printf '%s\n' "$value" > "$OUTPUT_DIR/$name.json"
+done
